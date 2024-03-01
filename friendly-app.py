6487385caeecfbe823b8dccdc109288c3a7c4e17
Ike Odi <iykeword@gmail.com>
2024-02-29 18:44:34 -0600

    commit

---
+++ friendly-app.py
@@ -0,0 +1,13 @@
+# app.py
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/', methods=['POST'])
+def hello():
+    name = request.form['name']
+    return f'Hello, {name}!'
